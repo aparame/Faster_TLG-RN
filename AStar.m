ebab8c9752347f3ec520785adcc67a86bf248989
Shahil Shaik <shahils@g.clemson.edu>
2024-03-19 16:40:28 -0400

    updated_astar

--- AStar.m
+++ AStar.m
@@ -1,20 +1,39 @@
-function astar_path_ex = AStar(model,map)
+function astar_path_ex = AStar(model,map,mapName)
+show(map);
 goals = model.goals;
 init_pos = model.init_pos;
+if strcmp('simpleMap',mapName)
+    init_pos_a_star = round([init_pos(:,1),map.GridSize(1,2)-init_pos(:,2)]);
+    for i=1:size(goals,3)
+        goals_poly = polyshape(goals(:,1,i),goals(:,2,i));
+        [goal_centre_x,goal_centre_y] = centroid(goals_poly);
+        goals_a_star(:,:,i) = round([goal_centre_x,map.GridSize(1,2)-goal_centre_y]);
 
-init_pos_a_star = round([init_pos(:,1),map.GridSize(1,2)-init_pos(:,2)]);
-for i=1:size(goals,3)
-    goals_poly = polyshape(goals(:,1,i),goals(:,2,i));
-    [goal_centre_x,goal_centre_y] = centroid(goals_poly);
-    goals_a_star(:,:,i) = round([goal_centre_x,map.GridSize(1,2)-goal_centre_y]);
-    if i==1
-        astar = plannerAStarGrid(map);
-        astar_path_ex = plan(astar,flip(init_pos_a_star),flip(goals_a_star(:,:,i)));
+        if i==1
+            astar = plannerAStarGrid(map);
+            astar_path_ex = plan(astar,flip(init_pos_a_star),flip(goals_a_star(:,:,i)));
 
-    else
-        astar = plannerAStarGrid(map);
-        astar_path_ex = [astar_path_ex;plan(astar,flip(goals_a_star(:,:,i-1)),flip(goals_a_star(:,:,i)))];
+        else
+            astar = plannerAStarGrid(map);
+            astar_path_ex = [astar_path_ex;plan(astar,flip(goals_a_star(:,:,i-1)),flip(goals_a_star(:,:,i)))];
+        end
+    end
+elseif strcmp('complexMap',mapName)
+    init_pos_a_star = round([map.GridSize(1,1)-init_pos(:,2),init_pos(:,1)]);
+    for i=1:size(goals,3)
+        goals_poly = polyshape(goals(:,1,i),goals(:,2,i));
+        [goal_centre_x,goal_centre_y] = centroid(goals_poly);
+        goals_a_star(:,:,i) = round([map.GridSize(1,1)-goal_centre_y,goal_centre_x]);
 
+        if i==1
+            astar = plannerAStarGrid(map);
+            astar_path_ex = plan(astar,init_pos_a_star,goals_a_star(:,:,i));
+            plot(astar_path_ex)
+        else
+            astar = plannerAStarGrid(map);
+            astar_path_ex = [astar_path_ex;plan(astar,goals_a_star(:,:,i-1),goals_a_star(:,:,i))];
+
+        end
     end
 end
 
--- main.m
+++ main.m
@@ -16,23 +16,21 @@ model.robot_r = 1/2; % 0.5m robot radius;
 % select_map{map_string, custom/inbuilt example, unknown obstacle trigger(0/1)}
 % astar_path is a function to calculate the Astar path based on the known
 % map, initial positions and the goal
-
-[map,map_LTL,model] = select_map(model,'complexMap','custom',0);   % 'simple' and 'custom/built-in'
+mapName = 'complexMap';
+[map,map_LTL,model] = select_map(model,mapName,'custom',0);   % 'simple' and 'custom/built-in'
 
 tic;
 [model,LTL_path] = LTL_Planning(model);
 model.LTL_path = LTL_path(3:end,:);
 model.LTL_path(end+1,:) = model.goals_XY(1,:);
+toc;
                                                 %%  Known Environment with A* %%
-% model.astar_path = AStar(model,map);
+model.astar_path = AStar(model,map_LTL,mapName);
 % model.astar_path = [model.astar_path(:,2),map.GridSize(1,2)-model.astar_path(:,1)];
 
-
-% % A Star Path Plotting (black)
-% plot(model.init_pos(1,1),model.init_pos(1,2),'.','MarkerSize',30);
-% arrow([model.astar_path(1:arrow_distance:end-1,1),model.astar_path(1:arrow_distance:end-1,2)],...
-%     [model.astar_path(2:arrow_distance:end,1),model.astar_path(2:arrow_distance:end,2)],2); % plot path as arrows
-
+%                                                 %%  Known Environment with RRT* %%
+% model.astar_path = AStar(model,map,mapName);
+% model.astar_path = [model.astar_path(:,2),map.GridSize(1,2)-model.astar_path(:,1)];
 
 
                                             %% Get Robustness and Prediction Horizon %%
@@ -47,6 +45,7 @@ model.hor_length = (size(model.LTL_path,1)-1)/model.ds;
 model.u0.min = model.u0.min(:,ones(1,model.hor_length+1)); % minimum control input ---> set in system description code
 model.u0.max = model.u0.max(:,ones(1,model.hor_length+1)); % maximum control input
 
+tic;
 % MILP Problem solver
 model = generate_STL_path(model,map);
 model.STL_path = model.solutions{1,3};
@@ -78,6 +77,7 @@ plot(LTL_path(2,1),LTL_path(2,2),'ko',LTL_path(end,1),LTL_path(end,2),'k^',...
 plot(LTL_path(size(model.goals,3)+1:end,1),LTL_path(size(model.goals,3)+1:end,2),'b','LineWidth',3);
 plot(model.init_pos(1,1),model.init_pos(1,2),'.','MarkerSize',30)
 plot(model.STL_path(1,:),model.STL_path(2,:),'r','LineWidth',3)
+plot(model.astar_path(:,2),map.GridSize(1,1)-model.astar_path(:,1),'--k','LineWidth',1.5)
 title('LTL and STL Safe Path Planning')
 
 hold off
